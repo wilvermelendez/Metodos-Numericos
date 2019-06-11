2b04d5c61e394b0f7793c847003e0e5423222160
Wilver Melendez <wilver.melendez@gmail.com>
2019-06-10 21:32:17 -0600

    Agregar polinomios

--- Periodo1/Unidad2/Cortos/P1_Lagrange_corto.m
+++ Periodo1/Unidad2/Cortos/P1_Lagrange_corto.m
@@ -10,21 +10,26 @@ f=input('Función f(x): ');
 fun=double(subs(f,datos));
 L=zeros(1,longitud);
 aproximado=0;
-
+polx=0;
 for i=1:longitud
     num=1;
+    numx=1;
     den=1;
     for j=1:longitud        
         if j~=i
             num=num*(valor-datos(j));
+            numx=numx*(x-datos(j));
             den=den*(datos(i)-datos(j));
         end
     end
     L(i)=num/den;
+    Lx(i)=numx/den;
     aproximado=aproximado+(L(i)*fun(i));
+    polx=polx+(Lx(i)*fun(i));
 end
 fun
 L
+pretty(simplify(polx))
 fprintf('P%1.0f(%3.3f)= %9.9f\n\n',double(longitud-1),double(valor),double(aproximado));
 %fprintf('Valor Exacto de la Función: %9.9f',double(subs(f,val)));
 %fprintf('\nError: %e\n\n',double(abs(pol-subs(f,val))));
